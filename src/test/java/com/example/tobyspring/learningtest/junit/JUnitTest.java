516d6b8f3590642c7c077399168e5d31663def11
kyuwon53 <lkw2v@naver.com>
2022-09-17 16:23:39 +0900

    add JUnitTest case

--- src/test/java/com/example/tobyspring/learningtest/junit/JUnitTest.java
+++ src/test/java/com/example/tobyspring/learningtest/junit/JUnitTest.java
@@ -1,30 +1,50 @@
 package com.example.tobyspring.learningtest.junit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
 
+@SpringBootTest
+@ContextConfiguration
 public class JUnitTest {
     static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+    static ApplicationContext contextObject = null;
+
+    @Autowired
+    ApplicationContext context;
 
     @Test
     public void test1() {
         assertThat(testObjects).isNotIn(this);
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context).isTrue();
+        contextObject = this.context;
     }
 
     @Test
     public void test2() {
         assertThat(testObjects).isNotIn(this);
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context).isTrue();
+        contextObject = this.context;
     }
 
     @Test
     public void test3() {
         assertThat(testObjects).isNotIn(this);
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context).isTrue();
+        contextObject = this.context;
     }
 }
